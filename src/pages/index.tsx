60c78e9a96533f9951cc3d9e89ead00bdb249281
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-02-13 22:11:10 +0530

    removed antd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,96 +1,94 @@
 
 import { Inter } from "next/font/google";
-import {  useState } from "react"
-import { Space, Table, Tag } from 'antd';
-import type { TableProps } from 'antd';
-import {  Input } from 'antd';
+ import {  useState } from "react"
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [email,setEmail]=useState("")
- interface DataType {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-}
+//  interface DataType {
+//   key: string;
+//   name: string;
+//   age: number;
+//   address: string;
+//   tags: string[];
+// }
 
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+// const columns: TableProps<DataType>['columns'] = [
+//   {
+//     title: 'Name',
+//     dataIndex: 'name',
+//     key: 'name',
+//     render: (text) => <a>{text}</a>,
+//   },
+//   {
+//     title: 'Age',
+//     dataIndex: 'age',
+//     key: 'age',
+//   },
+//   {
+//     title: 'Address',
+//     dataIndex: 'address',
+//     key: 'address',
+//   },
+//   {
+//     title: 'Tags',
+//     key: 'tags',
+//     dataIndex: 'tags',
+//     render: (_, { tags }) => (
+//       <>
+//         {tags.map((tag) => {
+//           let color = tag.length > 5 ? 'geekblue' : 'green';
+//           if (tag === 'loser') {
+//             color = 'volcano';
+//           }
+//           return (
+//             <Tag color={color} key={tag}>
+//               {tag.toUpperCase()}
+//             </Tag>
+//           );
+//         })}
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Action',
+//     key: 'action',
+//     render: (_, record) => (
+//       <Space size="middle">
+//         <a>Invite {record.name}</a>
+//         <a>Delete</a>
+//       </Space>
+//     ),
+//   },
+// ];
 
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
-const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
-  console.log('params', pagination, filters, sorter, extra);
-};
+// const data: DataType[] = [
+//   {
+//     key: '1',
+//     name: 'John Brown',
+//     age: 32,
+//     address: 'New York No. 1 Lake Park',
+//     tags: ['nice', 'developer'],
+//   },
+//   {
+//     key: '2',
+//     name: 'Jim Green',
+//     age: 42,
+//     address: 'London No. 1 Lake Park',
+//     tags: ['loser'],
+//   },
+//   {
+//     key: '3',
+//     name: 'Joe Black',
+//     age: 32,
+//     address: 'Sydney No. 1 Lake Park',
+//     tags: ['cool', 'teacher'],
+//   },
+// ];
+// const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+//   console.log('params', pagination, filters, sorter, extra);
+// };
 
   return (
     <main
@@ -100,14 +98,14 @@ const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter,
      
       
       
-        <Input className="lg:w-1/2 md:w-1/2 w-1/2" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Paste an email to be verified" />
+        <input type="text" className="lg:w-1/2 md:w-1/2 w-1/2 px-4 py-2 rounded shadow" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Paste an email to be verified" /> 
      
 
         <button className="bg-green-800  text-black px-4 py-1 rounded shadow">Verify</button>
      
 
 
-       <Table columns={columns} dataSource={data} onChange={onChange} />
+       {/* <Table columns={columns} dataSource={data} onChange={onChange} /> */}
         
      
     </main>
